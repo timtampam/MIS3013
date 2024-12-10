3e184d67d4e68976c98f90c3797a438a8bb4c58b
Anna Pham <112008731+timtampam@users.noreply.github.com>
2024-12-10 00:05:04 -0600

    Add files via upload

---
+++ Exam3_Student/Exam3_Student/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam3_Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            string stringGrade;
+            double doubleGrade;
+            bool isDouble;
+
+            // create student object
+            Student S1 = new Student();
+
+            // user input
+            Console.WriteLine("Please enter a student's first name");
+            S1.FirstName = Console.ReadLine();
+            Console.WriteLine("Please enter a student's last name");
+            S1.LastName = Console.ReadLine();
+            Console.WriteLine("Please enter a student's student ID in 4 digits (e.g. 1111)");
+            S1.StudentID = Convert.ToInt32(Console.ReadLine());
+
+            // input exam grades
+            for (int i = 1; i < 5; i++)
+            {
+                do
+                {
+                    Console.WriteLine($"\nPlease input {S1.FirstName}'s exam #{i} grade >>");
+                    stringGrade = Console.ReadLine();
+                    isDouble = Double.TryParse(stringGrade, out doubleGrade);
+
+                    if (isDouble == false) // invalid grade
+                    {
+                        Console.WriteLine($"{stringGrade} is not a valid grade. Please input again >>");
+                    }
+                    else if (doubleGrade < 0 || doubleGrade > 100) // grade not in range
+                    {
+                        Console.WriteLine($"{doubleGrade} is NOT between 0 and 100. Please input again >>");
+                    }
+                    else
+                    {
+                        S1.ExamGrades.Add(doubleGrade);
+                    }
+                } while (isDouble != true || doubleGrade < 0 || doubleGrade > 100);
+            }
+
+            // output exam grades evaluated
+            Console.WriteLine($"\n{S1.FirstName}'s exam grades range between {S1.GetMin(S1.ExamGrades)} and " +
+                $"{S1.GetMax(S1.ExamGrades)} with the average of {S1.GetAverage(S1.ExamGrades).ToString("N2")} " +
+                $"\n(as {S1.ExamWeight.ToString("P2")} of the final grade)");
+
+            // input homework grades
+            for (int i = 1; i < 6; i++)
+            {
+                do
+                {
+                    Console.WriteLine($"\nPlease input {S1.FirstName}'s homework #{i} grade >>");
+                    stringGrade = Console.ReadLine();
+                    isDouble = Double.TryParse(stringGrade, out doubleGrade);
+
+                    if (isDouble == false) // invalid grade
+                    {
+                        Console.WriteLine($"{stringGrade} is not a valid grade. Please input again >>");
+                    }
+                    else if (doubleGrade < 0 || doubleGrade > 100) // grade not in range
+                    {
+                        Console.WriteLine($"{doubleGrade} is NOT between 0 and 100. Please input again >>");
+                    }
+                    else
+                    {
+                        S1.HomeworkGrades.Add(doubleGrade);
+                    }
+                } while (isDouble != true || doubleGrade < 0 || doubleGrade > 100);
+            }
+
+            // output homework grades evaluated
+            Console.WriteLine($"\n{S1.FirstName}'s homework grades range between {S1.GetMin(S1.HomeworkGrades)} and " +
+                $"{S1.GetMax(S1.HomeworkGrades)} with the average of {S1.GetAverage(S1.HomeworkGrades).ToString("N2")} " +
+                $"\n(as {S1.HomeworkWeight.ToString("P2")} of the final grade)");
+
+            // input quiz grades
+            for (int i = 1; i < 6; i++)
+            {
+                do
+                {
+                    Console.WriteLine($"\nPlease input {S1.FirstName}'s quiz #{i} grade >>");
+                    stringGrade = Console.ReadLine();
+                    isDouble = Double.TryParse(stringGrade, out doubleGrade);
+
+                    if (isDouble == false) // invalid grade
+                    {
+                        Console.WriteLine($"{stringGrade} is not a valid grade. Please input again >>");
+                    }
+                    else if (doubleGrade < 0 || doubleGrade > 100) // grade not in range
+                    {
+                        Console.WriteLine($"{doubleGrade} is NOT between 0 and 100. Please input again >>");
+                    }
+                    else
+                    {
+                        S1.QuizGrades.Add(doubleGrade);
+                    }
+                } while (isDouble != true || doubleGrade < 0 || doubleGrade > 100);
+            }
+
+            // output homework grades evaluated
+            Console.WriteLine($"\n{S1.FirstName}'s quiz grades range between {S1.GetMin(S1.QuizGrades)} and " +
+                $"{S1.GetMax(S1.QuizGrades)} with the average of {S1.GetAverage(S1.QuizGrades).ToString("N2")} " +
+                $"\n(as {S1.QuizWeight.ToString("P2")} of the final grade)");
+
+            // output final grade
+            Console.WriteLine($"\n{S1.FirstName} {S1.LastName} ({S1.StudentID})'s final grade is {S1.GetFinalGrade().ToString("N2")}");
+
+            // output letter grade
+            Console.WriteLine($"\n{S1.FirstName} {S1.LastName} ({S1.StudentID})'s letter grade is {S1.GetLetterGrade()} with a GPA of {S1.GetGPA()}");
+
+            Console.ReadKey(); // end
+        }
+
+    }
+}
---
+++ Exam3_Student/Exam3_Student/Student.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam3_Student
+{
+    internal class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int StudentID { get; set; }
+
+        public int ExamCount { get; set; }
+
+        public double ExamWeight { get; set; }
+
+        public List<double> ExamGrades { get; set; }
+
+        public int HomeworkCount { get; set; }
+
+        public double HomeworkWeight { get; set; }
+
+        public List<double> HomeworkGrades { get; set; }
+
+        public int QuizCount { get; set; }
+
+        public double QuizWeight { get; set; }
+
+        public List<double> QuizGrades { get; set; }
+
+        public Student()
+        {
+            FirstName = "";
+            LastName = "";
+            StudentID = 0;
+            ExamCount = 4;
+            ExamWeight = .4;
+            ExamGrades = new List<double>();
+            HomeworkCount = 5;
+            HomeworkWeight = .3;
+            HomeworkGrades = new List<double>();
+            QuizCount = 5;
+            QuizWeight = .3;
+            QuizGrades = new List<double>();
+        }
+
+        public double GetAverage(List<double> list)
+        {
+            double avg = 0; 
+
+            foreach (double grade in list)
+            {
+                avg += grade;
+            }
+
+            avg /= list.Count;
+
+            return avg;
+        }
+
+        public double GetMax(List<double> list)
+        {
+            double max = 0;
+
+            foreach (double grade in list)
+            {
+                if (grade > max)
+                {
+                    max = grade;
+                }
+            }
+
+            return max;
+        }
+
+        public double GetMin(List<double> list)
+        {
+            double min = 100;
+
+            foreach (double grade in list)
+            {
+                if (grade < min)
+                {
+                    min = grade;
+                }
+            }
+
+            return min;
+        }
+
+        public double GetFinalGrade()
+        {
+            double final = (GetAverage(ExamGrades) * ExamWeight) + (GetAverage(HomeworkGrades) * HomeworkWeight) + 
+                (GetAverage(QuizGrades) * QuizWeight);
+
+            return final;
+        }
+
+        public double GetGPA()
+        {
+            double GPA = 0;
+
+            if (GetFinalGrade() >= 90)
+            {
+                GPA = 4;
+            }
+            else if (GetFinalGrade() >= 80)
+            {
+                GPA = 3;
+            }
+            else if (GetFinalGrade() >= 70)
+            {
+                GPA = 2;
+            }
+            else if (GetFinalGrade() >= 60)
+            {
+                GPA = 1;
+            }
+
+            return GPA;
+        }
+
+        public char GetLetterGrade()
+        {
+            char letter = 'F';
+
+            if (GetGPA() == 4)
+            {
+                letter = 'A';
+            }
+            else if (GetGPA() == 3)
+            {
+                letter = 'B';
+            }
+            else if (GetGPA() == 2)
+            {
+                letter = 'C';
+            }
+            else if (GetGPA() == 1)
+            {
+                letter = 'D';
+            }
+
+            return letter;
+        }
+    }
+}
